56b86e4243006786731a8de5836a29424859861a
Serghei Filippov <blastowind@gmail.com>
2012-01-31 00:15:09 -0500

    added groups to models.

--- persoa_main/models/__init__.py
+++ persoa_main/models/__init__.py
@@ -3,11 +3,13 @@ models relating to personality
 """
 
 from persoa_main.models.choice import BasicChoice, LinearChoice
+from persoa_main.models.group import TraitGroup
 from persoa_main.models.trait import BasicTrait, LinearTrait
 
 __all__ = [
     BasicChoice,
     LinearChoice,
+    TraitGroup,
     BasicTrait,
     LinearTrait,
 ]
