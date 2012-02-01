99256f9a5ddeb660d11e9b08fddc1d4b9cf13c4b
Serghei Filippov <blastowind@gmail.com>
2012-02-01 06:54:51 +0400

    Fixed up urls.

--- persoa_main/urls/details.py
+++ persoa_main/urls/details.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('persoa_main.views.details',
-    url(r'^choices\?', 'choices'),
-    url(r'^traits\?', 'traits'),
-    url(r'^trait_groups\?', 'trait_groups'),
+    url(r'choices', 'choices'),
+    url(r'traits', 'traits'),
+    url(r'trait_groups', 'trait_groups'),
 )
