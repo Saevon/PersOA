1f45a6ca7955a7b8d1accd42af0791c2094a8234
Serghei Filippov <blastowind@gmail.com>
2012-02-01 19:43:22 -0500

    added template functions for views.

---
+++ persoa_main/views/details.py
@@ -0,0 +1,9 @@
+
+def groups(request, poll_id):
+    pass
+
+def traits(request, poll_id):
+    pass
+
+def choices(request, poll_id):
+    pass
---
+++ persoa_main/views/generate.py
@@ -0,0 +1,9 @@
+
+def full(request, poll_id):
+    pass
+
+def group(request, poll_id):
+    pass
+
+def trait(request, poll_id):
+    pass
