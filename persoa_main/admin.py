9959c918105a93bd329abc8d2bf7f40bef4d96c7
Serghei Filippov <blastowind@gmail.com>
2012-02-01 19:31:46 -0500

    Added groups to admin.

--- persoa_main/admin.py
+++ persoa_main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from persoa_main.models.choice import BasicChoice, LinearChoice
+from persoa_main.models.groups import TraitGroup
 from persoa_main.models.trait import BasicTrait, LinearTrait
 
 class BasicChoiceInline(admin.TabularInline):
@@ -38,3 +39,4 @@ admin.site.register(LinearChoice)
 admin.site.register(BasicChoice)
 admin.site.register(BasicTrait, BasicTraitAdmin)
 
+admin.site.register(TraitGroup)
