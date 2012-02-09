ab9541e07eb05cb20b44508d1d4de73cefbf1b3f
Serghei Filippov <blastowind@gmail.com>
2012-02-08 20:37:33 -0500

    Added more admins.

--- persoa_main/admin.py
+++ persoa_main/admin.py
@@ -10,6 +10,12 @@ class BasicChoiceInline(admin.TabularInline):
     fields = ['name', 'defn']
     ordering = ['name']
 
+class SubChoiceInline(admin.TabularInline):
+    model = SubChoice
+    extra = 2
+    fields = ['name', 'defn']
+    ordering = ['name']
+
 class LinearChoiceInline(admin.TabularInline):
     model = LinearChoice
     extra = 1
@@ -33,10 +39,23 @@ class BasicTraitAdmin(admin.ModelAdmin):
     ]
     inlines = [BasicChoiceInline]
 
-admin.site.register(LinearTrait, LinearTraitAdmin)
-admin.site.register(LinearChoice)
+class BasicChoiceAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'defn']}),
+        (None, {'fields': ['desc']}),
+    ]
+    inlines = SubChoiceInline
 
-admin.site.register(BasicChoice)
+class LinearChoiceAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'defn']}),
+        (None, {'fields': ['desc', 'side']}),
+    ]
+
+admin.site.register(LinearTrait, LinearTraitAdmin)
+admin.site.register(LinearChoice, LinearChoiceAdmin)
+
+admin.site.register(BasicChoice, BasicChoiceAdmin)
 admin.site.register(SubChoice)
 admin.site.register(BasicTrait, BasicTraitAdmin)
 
