14898f12dbdc72d10012fa45d4fbf7194bfdcd6a
Serghei Filippov <blastowind@gmail.com>
2012-03-24 13:01:54 -0400

    First Attempt to extract request arguments.
     * Added helpers
     * There is a whitelist to limit args (needs refactoring)

--- persoa_main/views/details.py
+++ persoa_main/views/details.py
@@ -2,6 +2,8 @@ from django.views.decorators.http import require_GET
 from django.http import HttpResponse
 import simplejson
 
+from persoa_main.helpers import extract, field
+
 @require_GET
 def trait_groups(request):
     response = HttpResponse(mimetype='application/json')
@@ -14,9 +16,45 @@ def traits(request):
     simplejson.dump(request.GET, response)
     return response
 
+CHOICE_TYPES = ['basic', 'linear'];
+
+def is_str(val):
+    return isinstance(val, str)
+
 @require_GET
 def choices(request):
+    whitelist = [field(*item) for item in [
+        [
+            ['choice_name', 'name'],
+            'name',
+            str,
+            None,
+        ],
+        [
+            ['choice_type', 'type'],
+            'type',
+            str,
+            None,
+            lambda val: (val in CHOICE_TYPES),
+        ],
+        [
+            ['trait', 'trait_name'],
+            'trait',
+            str,
+            None,
+        ],
+        [
+            ['include'],
+            'include',
+            list,
+            None,
+            lambda val: (reduce(is_str, val, False)),
+        ],
+    ]]
+
+    params = extract(request.GET, whitelist)
+
     response = HttpResponse(mimetype='application/json')
-    simplejson.dump(request.GET, response)
+    simplejson.dump(params, response)
     return response
 
