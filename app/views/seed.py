71573a950911267026bf393410602f1e109b5811
Serghei Filippov <blastowind@gmail.com>
2012-08-30 19:06:44 -0400

    Added Seed, which is an updating int.  * Everytime you use it, it will have a new value  * This makes sure all the random logic is in one place  * This class isn't in use yet

---
+++ app/views/seed.py
@@ -0,0 +1,42 @@
+from random import randint
+
+class Seed(int):
+	"""
+	A random seed for randomizing
+	same limits as an int
+	"""
+
+	INT_MIN = -10
+	INT_MAX = 10
+
+	def __init__(self, num=None):
+		"""
+		Creates a new seed, if no number is passed in then every new number generated
+		isn't based on the previous
+		"""
+		if num is None:
+			self.__algorithm = Seed._randint
+		else:
+			self.__algorithm = Seed._new
+			self.__val == num
+	
+	def __get__(self):
+		"""
+		Returns the current seed and generates a new one
+		"""
+		seed = self.__val
+		self.__val = self.__algorithm(seed)
+
+		return seed
+
+	def _new(self, seed):
+		"""
+		Creates a new seed based on the given value
+		"""
+		return NotImplemented
+
+	def _randint(self, seed):
+		"""
+		Creates a new seed using random.randint
+		"""
+		return randint(Seed.INT_MIN, Seed.INT_MAX)
