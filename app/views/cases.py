770a81849d42135aea4bbdceff6be15b2be007aa
Serghei Filippov <blastowind@gmail.com>
2012-08-31 08:43:01 -0400

    Added a description to Cases. Moved location of INCLUDE_NAME to Case.

--- app/views/cases.py
+++ app/views/cases.py
@@ -2,20 +2,39 @@ from app.views.whitelist import Whitelist
 from helpers.decorators import cascade
 
 class Case(object):
+    """
+    A check that ensures that the given Whitelist has a given property
+    """
+
+    INCLUDE_NAME = Whitelist.INCLUDE_NAME
+
     def passed(self, params):
+        """
+        A boolean check that checks if params has the expected property
+        """
         raise NotImplementedError
 
 class IncludeCase(object):
+    """
+    A Case that ensures that the every prerequisite INCLUDES have been supplies
+        Use IncludeCase.key('field') to add expected fields
+    """
 
     def __init__(self, keys):
         self._keys = set(keys)
 
     @cascade
     def key(self, key):
+        """
+        Adds a new fields to check for, always makes sure ALL the fields have been included
+        """
         self._keys.add(key)
 
     def passed(self, params):
-        includes = params.get(Whitelist.INCLUDE_NAME, None)
+        """
+        A boolean check that checks if params has the expected include fields enabled
+        """
+        includes = params.get(Case.INCLUDE_NAME, None)
         if includes is None:
             includes = []
 
